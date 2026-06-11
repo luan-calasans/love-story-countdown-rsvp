95261b3b84f5ba25e8ad3011236e1930d00ba0d6
Luan Calasans <luan.calasans@outlook.com>
2026-06-11 11:00:23 -0300

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import PageLoader from "./components/PageLoader";
+import { Toaster } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+import PageLoader from './components/PageLoader';
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <PageLoader />
-      <Toaster />
-      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "") || undefined}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+	<QueryClientProvider client={queryClient}>
+		<TooltipProvider>
+			<PageLoader />
+			<Toaster />
+			<BrowserRouter
+				basename={import.meta.env.BASE_URL.replace(/\/$/, '') || undefined}
+			>
+				<Routes>
+					<Route path="/" element={<Index />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</TooltipProvider>
+	</QueryClientProvider>
 );
 
 export default App;
